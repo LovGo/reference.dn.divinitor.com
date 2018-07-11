050b2f97f90a37a9ba7a0fc37c8ab1cea863b7b1
Vincent Zhang <vincentzhang96@users.noreply.github.com>
2018-07-11 15:46:01 -0700

    Add support for more complex feature enabling, update feature enables (#22)
    * Add support for function enabled()
    
    * Exponential FD deprecation

--- src/api/features.js
+++ src/api/features.js
@@ -4,10 +4,20 @@ import Store from '@/store';
 
 let Features = {
     RegionTimeInfo: { regions: ["na"], enabled: true },
-    ResourceGameChat: { regions: ["na"], enabled: true },
-    ResourceServerStats: { regions: ["na"], enabled: true },
+    ResourceGameChat: { regions: ["na"], enabled: false },
+    ResourceServerStats: { regions: ["na"], enabled: false },
     ResourceDownloadGameClient: { regions: ["na"], enabled: true },
-    HideExpFd: { regions: ["ko"], enabled: true },
+    HideExpFd: {
+        regions: ["ko", "na", "sea", "eu"], 
+        enabled: {
+            ko: true,
+            sea: true,
+            eu: true,
+            na() {
+                return new Date() >= new Date(2018, 7, 12, 2);
+            },
+        }
+    },
 
     // Add features above this
 
@@ -19,7 +29,24 @@ let Features = {
 
         let regionName = Store.state.regionCode || this.getDefaultRegionCode();
         if (feature.regions.indexOf(regionName) != -1) {
-            return feature.enabled;
+            if (typeof(feature.enabled) === "boolean") {
+                return feature.enabled;
+            }
+            if (typeof(feature.enabled) === "function") {
+                return feature.enabled(regionName);
+            }
+            if (typeof(feature.enabled) === "object") {
+                const regionEnabled = feature.enabled[regionName];
+                if (regionEnabled == null) {
+                    return false;
+                }
+                if (typeof(regionEnabled) === "boolean") {
+                    return regionEnabled;
+                }
+                if (typeof(regionEnabled) === "function") {
+                    return regionEnabled();
+                }
+            }
         }
 
         return false;
