3cdcf54e9c493212c5fda3b650acac674b410a4a
Vincent Zhang <vzhang@purdue.edu>
2019-03-23 10:17:12 -0700

    Support critdmg v1

--- src/models/jobs/IJobScaling.ts
+++ src/models/jobs/IJobScaling.ts
@@ -1,4 +1,4 @@
-export default interface IJobScaling {
+export interface IJobScalingBase {
     jobId: number;
     vitToHp: number;
     strToPhys: number;
@@ -6,6 +6,20 @@ export default interface IJobScaling {
     intToMagic: number;
     vitToDef: number;
     intToMdef: number;
+    scalingVersion: undefined|0|1;
+}
+
+export interface IJobScalingV0 extends IJobScalingBase {
+    scalingVersion: undefined|0;
     agiToCrit: number;
     strIntToCritDmg: number;
 }
+
+export interface IJobScalingV1 extends IJobScalingBase {
+    scalingVersion: 1;
+    strToCritDmg: number;
+    agiToCritDmg: number;
+    intToCritDmg: number;
+}
+
+export type IJobScaling = IJobScalingBase | IJobScalingV0 | IJobScalingV1;
